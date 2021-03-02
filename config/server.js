6ac123adce13c185ffc91dc0b3376af705f4f63a
pinkman7009 <soumik.chaudhuri2000@gmail.com>
2021-03-02 10:39:30 +0530

    updated database config

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'ec6abd373e677509b53a54722fbe7f8e'),
+      secret: env("ADMIN_JWT_SECRET", "ec6abd373e677509b53a54722fbe7f8e"),
     },
   },
+  url: env("HEROKU_URL"),
 });
